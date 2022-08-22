f6bdc8117ca66693af8124d5c253104dd503af23
ItsTuukka <tuukka.ylostalo@helsinki.fi>
2022-08-22 14:15:22 +0300

    spelling fix

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -24,7 +24,7 @@ const Homeview = () => {
           information regarding that station.
           <br />
           There is also a list of journeys represented as a sortable data table.
-          Journyes can be searched by the departure or return station name
+          Journeys can be searched by the departure or return station name.
         </b>
       </div>
     </div>
